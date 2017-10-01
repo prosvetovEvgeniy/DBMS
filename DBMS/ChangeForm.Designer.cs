f02740637aabbefa7ab6e3cc02f8f9dfab1c0ea7
Prosvetov Evgeniy <prosvetov.work95@yandex.ru>
2017-10-01 16:44:14 +0700

    fix some bugs

--- DBMS/ChangeForm.Designer.cs
+++ DBMS/ChangeForm.Designer.cs
@@ -48,6 +48,7 @@
             // 
             this.dataGridView1.AllowUserToAddRows = false;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.EditMode = System.Windows.Forms.DataGridViewEditMode.EditOnEnter;
             this.dataGridView1.Location = new System.Drawing.Point(12, 12);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(657, 236);
